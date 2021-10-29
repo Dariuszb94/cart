c8cfcd2ee59df48b1514dc7488bd0f7b411e06f1
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-29 09:23:14 +0200

    wrapper styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import logo from "./logo.svg";
+import { useState } from "react";
+import { useQuery } from "react-query";
 
-function App() {
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
+import Wrapper from "./App.styles";
+const App = () => {
   return <div className="App"></div>;
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
+const client = new QueryClient();
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <QueryClientProvider client={client}>
+    <App />
+  </QueryClientProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
