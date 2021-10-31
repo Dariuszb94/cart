726d03ad954309137d093d09883d58d4017bfede
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-31 09:54:28 +0100

    cart item grid

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,37 @@ const App = () => {
   console.log(data);
   const getTotalItems = (items: CartItemType[]) =>
     items.reduce((ack: number, item) => ack + item.amount, 0);
-  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems((prev) => {
+      //Is the item already in the cart?
+      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
+      if (isItemInCart) {
+        return prev.map((item) =>
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1 }
+            : item
+        );
+      }
+      //First time the item is added
+      return [...prev, { ...clickedItem, amount: 1 }];
+    });
+  };
+  // const handleAddToCart = (clickedItem: CartItemType) => {
+  //   setCartItems((prev) => {
+  //     // 1. Is the item already added in the cart?
+  //     const isItemInCart = prev.find((item) => item.id === clickedItem.id);
+
+  //     if (isItemInCart) {
+  //       return prev.map((item) =>
+  //         item.id === clickedItem.id
+  //           ? { ...item, amount: item.amount + 1 }
+  //           : item
+  //       );
+  //     }
+  //     // First time the item is added
+  //     return [...prev, { ...clickedItem, amount: 1 }];
+  //   });
+  // };
   const handleRemoveFromCart = () => null;
   if (isLoading) return <LinearProgress />;
   if (error) return <div>Error</div>;
